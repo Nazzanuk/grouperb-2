6cac67181bf17eddcdea698b36840d812405b425
nazzanuk <nazzanuk@gmail.com>
2023-01-05 07:43:42 +0000

    app home color

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -84,6 +84,7 @@ export default function App({ Component, pageProps }: AppProps) {
 
         <link rel="icon" href="favicon.ico" />
 
+        <meta name="theme-color" content="#3a3a42"></meta>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
